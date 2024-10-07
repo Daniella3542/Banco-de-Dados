177dd328c98d1d53711b1543bc7342152b968432
DaniellaFernandes_ <168121886+Daniella3542@users.noreply.github.com>
2024-10-07 18:21:37 -0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+
+$db_host = "localhost";
+$db_user = "root";
+$db_pass = "";
+
+$db_name = "db_biblioteca";
+
+$conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
+
+if ($conn->connect_error){
+
+    die("conexão falhou:{$conn->connect_error}");
+}
+else{
+    echo "Parabéns!!! A conexão ao banco de dados ocorreu muito bem!";
+}
+
